6442f3ec57f2ec05ba3117082bf433a112c165a0
Jamie <jamiegrech@gmail.com>
2019-05-12 10:03:20 +0200

    Created Parser Class, changed TOK_TYPE enum values

---
+++ Parser.h
@@ -0,0 +1,14 @@
+//
+// Created by Jamie on 5/12/2019.
+//
+
+#ifndef COMPILERASSIGNMENT_PARSER_H
+#define COMPILERASSIGNMENT_PARSER_H
+
+
+class Parser {
+
+};
+
+
+#endif //COMPILERASSIGNMENT_PARSER_H
