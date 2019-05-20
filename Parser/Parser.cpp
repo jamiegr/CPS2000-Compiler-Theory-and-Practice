84335d4f6c122b8e22db80ec32b6295c2136299d
Jamie <jamiegrech@gmail.com>
2019-05-20 21:46:16 +0200

    Parser update 2

--- Parser/Parser.cpp
+++ Parser/Parser.cpp
@@ -8,11 +8,119 @@ Parser::Parser(Lexer lexer) {
     this->lexer = lexer;
 }
 
-Parser::~Parser() {
+std::vector<ASTNode *> *Parser::buildAST() {
+    std::vector<ASTNode*> *AST = new vector<ASTNode*>;
+    currentToken = lexer.getNextToken();
+    while(currentToken.getType() != TOK_EOF){
+        AST->push_back(parse());
+    }
+}
+
+ASTNode *Parser::parse() {
 
 }
 
-ASTNode *Parser::parseNode() {
+ASTBool *Parser::parseBool() {
+    return nullptr;
+}
+
+ASTFloat *Parser::parseFloat() {
+    return nullptr;
+}
+
+ASTFunctionCall *Parser::parseFunctionCall() {
+    return nullptr;
+}
+
+ASTIdentifierNode *Parser::parseIdentifierNode() {
+    return nullptr;
+}
+
+ASTInt *Parser::parseInt() {
+    return nullptr;
+}
+
+ASTSubExpression *Parser::parseSubExpression() {
+    return nullptr;
+}
+
+ASTUnaryNode *Parser::parseUnaryNode() {
+    return nullptr;
+}
+
+ASTActualParams *Parser::parseActualParams() {
+    return nullptr;
+}
+
+ASTAddOp *Parser::parseAddOp() {
+    return nullptr;
+}
+
+ASTExpression *Parser::parseExpression() {
+    return nullptr;
+}
+
+ASTFormalParam *Parser::parseFormalParam() {
+    return nullptr;
+}
+
+ASTFormalParams *Parser::parseFormalParams() {
+    return nullptr;
+}
+
+ASTMultOp *Parser::parseMultOp() {
+    return nullptr;
+}
+
+ASTRelOp *Parser::parseRelOp() {
+    return nullptr;
+}
+
+ASTSimpleExpression *Parser::parseSimpleExpression() {
+    return nullptr;
+}
+
+ASTTerm *Parser::parseTerm() {
+    return nullptr;
+}
+
+ASTType *Parser::parseType() {
+    return nullptr;
+}
+
+ASTUnaryOp *Parser::parseUnaryOp() {
+    return nullptr;
+}
+
+ASTAssignmentStatement *Parser::parseAssignmentStatement() {
+    return nullptr;
+}
+
+ASTBlockStatement *Parser::parseBlockStatement() {
+    return nullptr;
+}
+
+ASTForStatement *Parser::parseForStatement() {
+    return nullptr;
+}
+
+ASTFunctionDeclStatement *Parser::parseFunctionDeclStatement() {
+    return nullptr;
+}
+
+ASTIfStatement *Parser::parseIfStatement() {
+    return nullptr;
+}
+
+ASTPrintStatement *Parser::parsePrintStatement() {
+    return nullptr;
+}
+
+ASTReturnStatement *Parser::parseReturnStatement() {
+    return nullptr;
+}
+
+ASTVariableDeclStatement *Parser::parseVariableDeclStatement() {
     return nullptr;
 }
 
--- Parser/Parser.h
+++ Parser/Parser.h
@@ -8,15 +8,85 @@
 
 #include "../Lexer/Lexer.h"
 #include "AST/ASTNode.h"
+#include "AST/Expressions/Factors/ASTBool.h"
+#include "AST/Expressions/Factors/ASTFloat.h"
+#include "AST/Expressions/Factors/ASTFunctionCall.h"
+#include "AST/Expressions/Factors/ASTInt.h"
+#include "AST/Expressions/Factors/ASTSubExpression.h"
+#include "AST/Expressions/Factors/ASTUnaryNode.h"
+#include "AST/Expressions/ASTFormalParam.h"
+#include "AST/Expressions/ASTFormalParams.h"
+#include "AST/Statements/ASTAssignmentStatement.h"
+#include "AST/Statements/ASTBlockStatement.h"
+#include "AST/Statements/ASTForStatement.h"
+#include "AST/Statements/ASTFunctionDeclStatement.h"
+#include "AST/Statements/ASTIfStatement.h"
+#include "AST/Statements/ASTReturnStatement.h"
+#include "AST/Statements/ASTPrintStatement.h"
 
 class Parser {
 public:
     explicit Parser(Lexer lexer);
-    ~Parser();
-    ASTNode *parseNode();
+
+    vector<ASTNode *> *buildAST();
+
 private:
     Lexer lexer;
-    Token currentToken, nextToken;
+    Token currentToken;
+
+    ASTNode *parse();
+
+    ASTBool *parseBool();
+
+    ASTFloat *parseFloat();
+
+    ASTFunctionCall *parseFunctionCall();
+
+    ASTIdentifierNode *parseIdentifierNode();
+
+    ASTInt *parseInt();
+
+    ASTSubExpression *parseSubExpression();
+
+    ASTUnaryNode *parseUnaryNode();
+
+    ASTActualParams *parseActualParams();
+
+    ASTAddOp *parseAddOp();
+
+    ASTExpression *parseExpression();
+
+    ASTFormalParam *parseFormalParam();
+
+    ASTFormalParams *parseFormalParams();
+
+    ASTMultOp *parseMultOp();
+
+    ASTRelOp *parseRelOp();
+
+    ASTSimpleExpression *parseSimpleExpression();
+
+    ASTTerm *parseTerm();
+
+    ASTType *parseType();
+
+    ASTUnaryOp *parseUnaryOp();
+
+    ASTAssignmentStatement *parseAssignmentStatement();
+
+    ASTBlockStatement *parseBlockStatement();
+
+    ASTForStatement *parseForStatement();
+
+    ASTFunctionDeclStatement *parseFunctionDeclStatement();
+
+    ASTIfStatement *parseIfStatement();
+
+    ASTPrintStatement *parsePrintStatement();
+
+    ASTReturnStatement *parseReturnStatement();
+
+    ASTVariableDeclStatement *parseVariableDeclStatement();
 };
 
 
